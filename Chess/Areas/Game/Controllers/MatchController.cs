6071ea6881aeb7549a8aa64cd9b35505c9c2e5bb
voron <voron@LAPTOP-GT3KOQPJ>
2023-10-25 20:42:15 +0300

    25.10 Alpha

---
+++ Chess/Areas/Game/Controllers/MatchController.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System.Security.Claims;
+
+namespace Chess.Areas.Game.Controllers
+{
+    [Area("Game")]
+    [Authorize]
+    public class MatchController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public MatchController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public IActionResult Index(string id)
+		{
+			var session = _unitOfWork.Session.Get(a => a.Id == id);
+
+			Response.Cookies.Append("whiteId", session.WhiteId);
+			Response.Cookies.Append("blackId", session.BlackId);
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            Response.Cookies.Append("userId", userId);
+
+			return View();
+		}
+	}
+}
