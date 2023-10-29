7a47f9b0d21c95a539859aa0ec011c55e4e899a4
MaximKotelok <98751837+MaximKotelok@users.noreply.github.com>
2023-10-29 12:35:08 +0300

    Profile moved

--- Chess/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ Chess/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -17,7 +17,7 @@ namespace  Chess.Areas.Identity.Pages.Account.Manage
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public static string Index => "Index";
+        public static string EditProfile => "Index";
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -65,7 +65,8 @@ namespace  Chess.Areas.Identity.Pages.Account.Manage
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+        
+        public static string EditProfileNavClass(ViewContext viewContext) => PageNavClass(viewContext, EditProfile);
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
